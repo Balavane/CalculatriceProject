64a0ac571bcdcbf9795a425ebec44fe8d07b544d
balavane <danielbashonga88@gmail.com>
2023-09-27 14:16:07 +0200

    version 00

---
+++ index.js
@@ -0,0 +1,19 @@
+/* Dave tu fais la fonction add, multiplication*/
+const Add = ()=>{
+
+}
+const Mult = ()=>{
+    
+}
+/* Moi je veux faire la soustration et la division */
+const Soustr = ()=>{
+    
+}
+const Div = ()=>{
+    
+}
+
+/* La fonction pricipale on le fait à 2 et puis on vas voir quoi implemente*/
+const Calculatrice = ()=>{
+    
+}
