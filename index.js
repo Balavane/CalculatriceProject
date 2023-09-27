4f3613f9368f98d9b4cb79abffff54172f108cf9
balavane <danielbashonga88@gmail.com>
2023-09-27 15:18:27 +0200

    version 01

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const Mult = ()=>{
     
 }
 /* Moi je veux faire la soustration et la division */
-const Soustr = ()=>{
+const Soustr = (a,b)=>{
+    return a - b;
     
 }
-const Div = ()=>{
-    
+const Div = (a,b)=>{
+    return a / b;
 }
 
 /* La fonction pricipale on le fait à 2 et puis on vas voir quoi implemente*/
-const Calculatrice = ()=>{
-    
+const Calculatrice = (a,b,opera)=>{
+    if(opera == "Soustration"){
+        return Soustr(a,b)
+    }else if(opera == "Division"){
+        return Div(a,b)
+    }else{
+        return "Desole vous devez  entre le nombre 1 et nombre 2 puis choisir une operation"
+    }
 }
+
+console.log(Calculatrice(2,6, "Division"))
